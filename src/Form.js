bc3fa50548b6b8de70dc6056066783b98f8e39a9
Hanna Borodina <Hanna Borodina>
2017-01-12 14:51:51 +0200

    modified form component

--- src/Form.js
+++ src/Form.js
@@ -1,20 +1,14 @@
 import React, { Component } from 'react';
 import firebase from 'firebase';
 
-let textInput1 = null;
-let textInput2 = null;
-
-
 class Form extends Component {
   constructor(props) {
     super(props);
     this.state = {
       pickUpAddress: null,
       dropOffAddress: null,
-      lat1: null,
-      lng1: null,
-      lat2: null,
-      lng2: null
+      pickUpLatLng: null,
+      dropOffLatLng: null,
     };
   }
   componentWillMount() {
@@ -27,19 +21,19 @@ class Form extends Component {
   }
 
   componentDidMount() {
-    var input = document.getElementById('dropoff');
-    new window.google.maps.places.Autocomplete(input);
-    var input2 = document.getElementById('pickup');
+    const input1 = document.getElementById('dropoff');
+    new window.google.maps.places.Autocomplete(input1);
+    const input2 = document.getElementById('pickup');
     new window.google.maps.places.Autocomplete(input2);
   }
 
   btnOnclick(e) {
     e.preventDefault();
-    var k = new window.google.maps.LatLng(this.state.lat1, this.state.lng1);
-    var p = new window.google.maps.LatLng(this.state.lat2, this.state.lng2);
-    var distanceMetre = window.google.maps.geometry.spherical.computeDistanceBetween(k, p);
-    var distanceMiles = distanceMetre * 0.000621
-    var rootRef = firebase.database();
+    let distanceMetre = window.google.maps.geometry.spherical.computeDistanceBetween(this.state.pickUpLatLng, this.state.dropOffLatLng);
+    let distanceMiles = distanceMetre * 0.000621;
+    console.log(distanceMetre);
+    console.log(distanceMiles);
+    let rootRef = firebase.database();
     if (distanceMiles < 20) {
       rootRef.ref('distances/' + this.uuid()).set({
         distance: distanceMiles,
@@ -51,24 +45,22 @@ class Form extends Component {
   }
 
   uuid() {
-    var uuid = '';
+    let uuid = '';
     uuid += new Date().getTime();
     return uuid;
   }
 
   onChange1() {
-    var self = this;
-    var address1 = textInput1.value;
-    var geocoder = new window.google.maps.Geocoder();
+    let self = this;
+    let address1 = document.getElementById('pickup').value;
+    let geocoder = new window.google.maps.Geocoder();
     geocoder.geocode({ 'address': address1 }, function handleResults(results, status) {
       if (status === window.google.maps.GeocoderStatus.OK) {
-        var _pickUpAddress = results[0].formatted_address;
-        var _lat1 = results[0].geometry.location.lat();
-        var _lng1 = results[0].geometry.location.lng();
+        let _pickUpLatLng = results[0].geometry.location;
+        let _pickUpAddress = results[0].formatted_address;
         self.setState({
+          pickUpLatLng: _pickUpLatLng,
           pickUpAddress: _pickUpAddress,
-          lat1: _lat1,
-          lng1: _lng1
         });
       }
     });
@@ -76,18 +68,16 @@ class Form extends Component {
   }
 
   onChange2() {
-    var self = this;
-    var address2 = textInput2.value;
-    var geocoder = new window.google.maps.Geocoder();
+    let self = this;
+    let address2 = document.getElementById('dropoff').value;
+    let geocoder = new window.google.maps.Geocoder();
     geocoder.geocode({ 'address': address2 }, function (results, status) {
       if (status === window.google.maps.GeocoderStatus.OK) {
-        var _dropOffAddress = results[0].formatted_address;
-        var _lat2 = results[0].geometry.location.lat();
-        var _lng2 = results[0].geometry.location.lng();
+        let _dropOffLatLng = results[0].geometry.location;
+        let _dropOffAddress = results[0].formatted_address;
         self.setState({
+          dropOffLatLng: _dropOffLatLng,
           dropOffAddress: _dropOffAddress,
-          lat2: _lat2,
-          lng2: _lng2
         });
       }
     });
@@ -98,18 +88,14 @@ class Form extends Component {
     return (
       <form>
         <label>
-          Pick-up address:
-          <input className="form-control" type="text" ref={(input) => { textInput1 = input; } } id="pickup" size="50"
-            placeholder="input address" onBlur={this.onChange1.bind(this)} />
+          Pick up address:
+          <input className="form-control" type="text" id="pickup" size="50" placeholder="input address" onBlur={this.onChange1.bind(this)} />
         </label><br />
         <label>
           Drop off address:
-          <input className="form-control" type="text" ref={(input) => { textInput2 = input; } } id="dropoff" size="50"
-            placeholder="input address" onBlur={this.onChange2.bind(this)} />
+          <input className="form-control" type="text" id="dropoff" size="50" placeholder="input address" onBlur={this.onChange2.bind(this)} />
         </label><br />
         <button className="btn btn-info" onClick={this.btnOnclick.bind(this)}>Calculate distance</button>
-        <p>{this.state.lat1} {this.state.lng1} </p>
-        <p>{this.state.lat2} {this.state.lng2} </p>
         <p id="textPush"></p>
       </form>
     );
